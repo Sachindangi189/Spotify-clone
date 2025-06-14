497e1ccbb48b06830d5f1e236c51e59c137a740e
sachin <sachindangi783@gmail.com>
2025-06-14 10:11:46 +0530

    Add initial implementation of Spotify web player with HTML, CSS, and JavaScript

---
+++ app.js
@@ -0,0 +1,49 @@
+// let todo = [];
+// let choice = prompt("enter your choice");
+
+// while(true){
+//     if(choice === "quit"){
+//         console.log("user is quitting");
+//         break;
+//     }
+//     if(choice === "list"){
+//         console.log("--------------");
+//         for(i=0;i<todo.length;i++){
+//           console.log(i,todo[i]);
+//         }
+//         console.log("--------------");
+//     } else if(choice === "add"){
+//         let task = prompt("enter your element to add");
+//         todo.push(task);
+//         console.log("added successfully");
+//     } else if(choice === "delete"){
+//         let del = prompt("enter your index no. of task to delete");
+//         todo.splice(del,1);
+//         console.log("delete successfully");
+//     } else{
+//         console.log("wrong choice");
+//     }
+//     choice = prompt("enter your choice");
+// }
+
+//Assignment
+// let arry = [1,2,3,4,5,6,2,3];
+// let num = 2;
+// for(i=0;i<arry.length;i++){
+//     if(arry[i] == num){
+//         arry.splice(i,1);
+//     }
+   
+// }
+// console.log(arry);
+
+
+// function dice (){
+// let random = Math.floor(Math.random()*6)+1;
+// console.log(random);
+// }
+
+// dice();
+
+
+
